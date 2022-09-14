3b604e162c835670fcc5790ef1a793f69b4029fb
Augusto Soto <sotoaugustoa@gmail.com>
2022-09-14 11:10:48 -0500

    first commit

---
+++ 002_variables/variables.py
@@ -0,0 +1,26 @@
+# No se declaran los tipos de variables
+# String
+myVariable = "Hola desde una variable en Python"
+otraVariable = "Hello world!"
+
+print(myVariable)
+print(otraVariable)
+
+
+#int
+
+miNumero = 99999.9
+
+print(miNumero)
+
+#suma de variables
+
+x = 10
+y = 2
+z = x + y
+
+print(x)
+print(y)
+
+print("La suma de X y Y es: ")
+print(z)
